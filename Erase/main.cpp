64f8adf5af79018f466a089f89690b632694bbd7
Grebe_cq71@top-academy.ru <Grebe_cq71@top-academy.ru>
2025-06-03 13:14:35 +0300

    Ternar_ChessBoard

---
+++ Erase/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int size;
+	cout << "Введите размер доски: "; cin >> size;
+	int counter = 1;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			counter % 2 != 0 ? cout << "+ " : cout << "- ";
+			counter++;
+		}
+		cout << endl;
+	}
+}
